c0f43752a3db5788bdca9dda24b191c98c0b2c89
Chase McClain <iamchasem.dev@gmail.com>
2023-02-28 17:15:39 -0700

    completed exercise

--- app.js
+++ app.js
@@ -2,3 +2,26 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+ 
+let searchBttn = document.getElementById("submitSearch");
+let searchInput = document.getElementById("searchWord");
+let feedbackEle = document.getElementById("feedback");
+let img = document.querySelector("img");
+
+searchBttn.addEventListener("click", () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=Ezv8j3Zw0iHijLXfJ00OWt3FF2FOhFF1&s=" +
+    searchInput.value,
+    { mode: "cors" }
+  )
+  .then ((response) => response.json())
+  .then((res) => {
+    feedbackEle.textContent = "";
+    img.src = res.data.images.original.url;
+    searchInput.value = "";
+  })
+  .catch((err) => {
+    console.error(err);
+    feedbackEle.textContent = err.message;
+  });
+});
